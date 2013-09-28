22d81d92c1c4d841033558cd566d38a04c3b6f52
Alexander <alexanderjimenez@machentertainment.com>
2013-09-28 18:59:58 -0400

    Added Config Implemented Vault dependency

--- RPlite/src/com/machentertainment/RPlite/RPlite.java
+++ RPlite/src/com/machentertainment/RPlite/RPlite.java
@@ -1,7 +1,13 @@
 package com.machentertainment.RPlite;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -9,13 +15,34 @@ import org.bukkit.entity.Player;
 
 public final class RPlite extends JavaPlugin{
 	
+	Boolean verbose;
+	private static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
+	
 	@Override
 	public void onEnable() {
 		getLogger().info("RPlite is now starting.");
 		
+		if(this.getConfig().getBoolean("Verbose") == true) {
+			verbose = true;
+		}else{
+			verbose = false;
+		}
+		
 		
 		//Start getting commands
 		getCommand("mach").setExecutor(new RPliteCommandExecutor(this));
+		
+		//Vault
+		if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        setupPermissions();
+        setupChat();
 		
 		
 	}
@@ -25,6 +52,31 @@ public final class RPlite extends JavaPlugin{
 		getLogger().info("RPLite has been disabled");
 	}
 	
+	//Vault
+	private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+	
 	//Preformated message sending.
 	public void sendPlayer(Player player, String message){
 		player.sendMessage("[RPlite]: " + message);
@@ -33,12 +85,13 @@ public final class RPlite extends JavaPlugin{
 	//Console Logging (Config based)
 	public void sendLog(String level, String Message){
 		
-		if(con)
-			if(level.equalsIgnoreCase(info)){
+		if(verbose = true){
+			if(level.equalsIgnoreCase("info")){
 				getLogger().info(Message);
 			}else{
 				getLogger().warning(Message);
 			}
+		}
 	}
 
 }
