21c4bbfe98e7480daaeb3da8c56899206268efee
Kyle <Kyle@KylesComputer.home>
2013-09-28 16:40:38 -0700

    Fixed formating. Added formating to RPlite, under sendPlayer.

--- RPlite/src/com/machentertainment/RPlite/RPliteCommandExecutor.java
+++ RPlite/src/com/machentertainment/RPlite/RPliteCommandExecutor.java
@@ -27,74 +27,74 @@ public class RPliteCommandExecutor implements CommandExecutor{
 			}
 			if(args.length == 1){
 				if(args[1].equalsIgnoreCase("join")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Select a class to join.  Type /Mach classes to see the list.");
+					plugin.sendPlayer(playerObj, "Select a class to join.  Type /Mach classes to see the list.");
 				}
 				//TODO
 			}
 			if(args.length == 1){
 				if(args[1].equalsIgnoreCase("classes")){
 					plugin.sendPlayer(playerObj, ChatColor.GOLD + "____RPLite Class List____");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Class - Description");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Baker - (MORE INFO HERE)");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Banker - (MORE INFO HERE)");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Blacksmith - (MORE INFO HERE)");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Farmer - (MORE INFO HERE)");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Merchant - (MORE INFO HERE)");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Noble - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Class - Description");
+					plugin.sendPlayer(playerObj, "Baker - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Banker - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Blacksmith - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Farmer - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Merchant - (MORE INFO HERE)");
+					plugin.sendPlayer(playerObj, "Noble - (MORE INFO HERE)");
 				}
 				//TODO
 			}
 			if(args.length == 1){
 				if(args[1].equalsIgnoreCase("help")){
 					plugin.sendPlayer(playerObj, ChatColor.GOLD + "____RPLite Help Menu____");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Command: Description");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "/mach: Does something when we code it to");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "/mach help: Displays help menu");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "/mach classes: Lists classes");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "/mach join <class>: Joins a class");
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "/mach leaveclass: Leaves current class");
+					plugin.sendPlayer(playerObj, "Command: Description");
+					plugin.sendPlayer(playerObj, "/mach: Does something when we code it to");
+					plugin.sendPlayer(playerObj, "/mach help: Displays help menu");
+					plugin.sendPlayer(playerObj, "/mach classes: Lists classes");
+					plugin.sendPlayer(playerObj, "/mach join <class>: Joins a class");
+					plugin.sendPlayer(playerObj, "/mach leaveclass: Leaves current class");
 				}
 				//TODO
 			}
 			if(args.length == 1){
 				if(args[1].equalsIgnoreCase("leaveclass")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully left your class!");
+					plugin.sendPlayer(playerObj, "Successfully left your class!");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Blacksmith")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Blacksmith");
+					plugin.sendPlayer(playerObj, "Successfully joined Blacksmith");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Farmer")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Farmer");
+					plugin.sendPlayer(playerObj, "Successfully joined Farmer");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Merchant")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Merchant");
+					plugin.sendPlayer(playerObj, "Successfully joined Merchant");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Baker")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Baker");
+					plugin.sendPlayer(playerObj, "Successfully joined Baker");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Banker")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Banker");
+					plugin.sendPlayer(playerObj, "Successfully joined Banker");
 				}
 				//TODO
 			}
 			if(args.length == 2){
 				if(args[2].equalsIgnoreCase("Noble")){
-					plugin.sendPlayer(playerObj, ChatColor.GREEN + "Successfully joined Noble");
+					plugin.sendPlayer(playerObj, "Successfully joined Noble");
 				}
 				//TODO
 			}
