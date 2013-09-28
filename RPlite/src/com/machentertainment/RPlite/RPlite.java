8627495d1326897765ec2548f6a4e5c1984f3672
Alexander <alexanderjimenez@machentertainment.com>
2013-09-28 17:58:14 -0400

    Added basic /Mach command.

--- RPlite/src/com/machentertainment/RPlite/RPlite.java
+++ RPlite/src/com/machentertainment/RPlite/RPlite.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 public final class RPlite extends JavaPlugin{
 	
@@ -14,9 +15,18 @@ public final class RPlite extends JavaPlugin{
 		
 		
 		//Start getting commands
-		getCommand("mach").setExecutor(new RPliteCommandExecutor);
+		getCommand("mach").setExecutor(new RPliteCommandExecutor(this));
 		
 		
+	}
+	@Override
+	public void onDisable(){
+		getLogger().info("RPLite has been disabled");
+	}
+	
+	//Preformated message sending.
+	public void sendPlayer(Player player, String message){
+		player.sendMessage("[RPlite]: " + message);
 	}
 
 }
--- RPlite/src/com/machentertainment/RPlite/RPliteCommandExecutor.java
+++ RPlite/src/com/machentertainment/RPlite/RPliteCommandExecutor.java
@@ -1,5 +1,32 @@
 package com.machentertainment.RPlite;
 
-public class RPliteCommandExecutor {
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+
+public class RPliteCommandExecutor implements CommandExecutor{
+	
+	private RPlite plugin;
+	
+	public RPliteCommandExecutor(RPlite plugin){
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public boolean onCommand (org.bukkit.command.CommandSender sender,org.bukkit.command.Command cmd, String label, String[] args){
+		
+		if(cmd.getName().equalsIgnoreCase("Mach")) {
+			
+			String playerName = sender.getName();
+			Player playerObj = plugin.getServer().getPlayer(playerName);
+			
+			if(args.length == 0){
+				plugin.sendPlayer(playerObj, "Under Construction");
+				
+				return true;
+			}
+		}
+		
+		return false;
+	}
 
 }
