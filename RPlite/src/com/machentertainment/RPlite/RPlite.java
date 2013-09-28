9a92079f8da20408642ebf251bd61861fb71bbe9
Alexander <alexanderjimenez@machentertainment.com>
2013-09-28 17:32:25 -0400

    Started command executors.

--- RPlite/src/com/machentertainment/RPlite/RPlite.java
+++ RPlite/src/com/machentertainment/RPlite/RPlite.java
@@ -1,5 +1,26 @@
 package com.machentertainment.RPlite;
 
-public class RPlite {
+import java.util.List;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public final class RPlite extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("RPlite is now starting.");
+		
+		
+		//Start getting commands
+		getCommand("phoenix").setExecutor(new RPliteCommandExecutor(this));
+		getCommand("phoenix status").setExecutor(new RPliteCommandExecutor (this));
+		getCommand("phoenix join").setExecutor(new RPliteCommandExecutor(this));
+		getCommand("phoenix list").setExecutor(new RPliteCommandExecutor(this));
+		getCommand("phoenix version").setExecutor(new RPliteCommandExecutor(this));
+		
+		
+	}
 
 }
