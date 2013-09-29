17584dc9ae308e4b8b38fc875e06d65048efb50a
Kyle <Kyle@KylesComputer.home>
2013-09-28 19:05:57 -0700

    Added RPliteBlockBreakListener.java

---
+++ RPlite/src/com/machentertainment/RPlite/RPliteBlockBreakListener.java
@@ -0,0 +1,27 @@
+package com.machentertainment.RPlite;
+ 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+ 
+public class RPliteBlockBreakListener implements Listener {
+       
+        private RPlite plugin;
+       
+        public RPliteBlockBreakListener(RPlite instance) {
+                plugin = instance;
+        }
+ 
+        @EventHandler
+        public void onBlockBreak(BlockBreakEvent event, World world){
+                Player player = event.getPlayer();
+                Block block = event.getBlock();
+                
+        }
+ 
+}
